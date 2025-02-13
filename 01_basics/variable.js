5308e993f5b9ac3ec251cb79ba87156fac7e38da
sourodip19 <149153226+sourodip19@users.noreply.github.com>
2025-02-13 15:53:27 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "sourodip@123.com";
+var accountPass = 98765;
+accountcity = "Jaipur";
+let accountstate;
+
+// accountId = 54321;
+
+accountEmail = "sd@123gmail.com";
+accountpass = 56789;
+accountcity = "kolkata";
+
+console.table([accountId,accountEmail,accountPass,accountcity,accountstate]);
